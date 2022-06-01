28f1bfb45ae7bd24b54d4afb1cdad2fd0fa84545
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-02 02:36:53 +0300

    master: add task 9.3.2, 9.3.3

--- Task_9_1_3/Task_9_3_3/Program.cs
+++ Task_9_1_3/Task_9_3_3/Program.cs
@@ -7,10 +7,12 @@ namespace Task_9_3_2
         public delegate int Func(int x, int y);
         static void Main(string[] args)
         {
-            Func func = function;
-            Console.WriteLine( "5 - 6 = {0}", func(5, 6) ); ;
+            Func func = Function;
+            Console.WriteLine("5 - 6 = {0}", func.Invoke(5, 6));
+            Console.WriteLine("5 - 6 = {0}", func(5, 6));
+
         }
-        static int function(int a, int b)
+        static int Function(int a, int b)
         {
             return a - b;
         }
