d4a4b92929d608db1bafe424c7af42b70e36cbfb
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-02 02:11:16 +0300

    master: add 9.3.2

---
+++ Task_9_1_3/Task_9_2_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_9_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new RankException("Ошибка");
+            }
+            catch(Exception ex) when (ex is RankException)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Task_9_1_3/Task_9_3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_9_3_2
+{
+    internal class Program
+    {
+        public delegate int Func(int x, int y);
+        static void Main(string[] args)
+        {
+            Func func = Function;
+            Console.WriteLine( "5 - 6 = {0}", func(5, 6));
+        }
+        static int Function(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
