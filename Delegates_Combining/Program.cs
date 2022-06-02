2792d94ec90a230beb8df739f244928da597cc49
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-03 00:36:31 +0300

    master: add task 9.3.8, 9.3.8

---
+++ Delegates_Combining/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+    public delegate void ShowDelegate();
+    static void Main(string[] args)
+    {
+
+        ShowDelegate showDelegate1 = ShowMessage1;
+        showDelegate1 += ShowMessage2;
+
+        ShowDelegate showDelegate2 = ShowMessage3;
+        showDelegate2 += ShowMessage4;
+
+        ShowDelegate showDelegate3 = showDelegate1 + showDelegate2;
+
+        showDelegate3.Invoke();
+
+        Console.ReadKey();
+    }
+
+    static void ShowMessage1()
+    {
+        Console.WriteLine("Метод 1");
+    }
+
+    static void ShowMessage2()
+    {
+        Console.WriteLine("Метод 2");
+    }
+
+    static void ShowMessage3()
+    {
+        Console.WriteLine("Метод 3");
+    }
+
+    static void ShowMessage4()
+    {
+        Console.WriteLine("Метод 4");
+    }
+}
---
+++ Task_9_3_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Generic_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke();
+
+            Func <int, int, int, int> sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 30, 120);
+            Console.WriteLine(result);
+
+            Predicate <string> checkLengthDelegate = CheckLength;
+            bool status = checkLengthDelegate.Invoke("skill_factory");
+            Console.WriteLine(status);
+        }
+        static void ShowMessage()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3) return true;
+            return false;
+        }
+    }
+}
