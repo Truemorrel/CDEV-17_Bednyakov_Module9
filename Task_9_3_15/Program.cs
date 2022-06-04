c08efc0516b69b1355dbec93192fd94ae3ef1b05
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-04 20:50:45 +0300

    master: add task 9.3.15

---
+++ Task_9_3_15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_9_3_15
+{
+    class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = () =>
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+    }
+}
