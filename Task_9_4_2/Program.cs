c7e5e821f559397812dff343699677d0682d36c9
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-04 22:58:32 +0300

    master: add task 9.4.2

---
+++ Task_9_4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_9_4_2
+{
+    internal class Program
+    {
+        delegate Car MotorDelegate();
+        public static Car CarMethod()
+        {
+            return null;
+        }
+        public static Lexus lexusMethod()
+        {
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            MotorDelegate carDelegate = CarMethod;
+            MotorDelegate lexusDelegate = lexusMethod;
+            Console.ReadKey();
+        }
+    }
+
+    class Car { };
+    class Lexus : Car { };
+}
