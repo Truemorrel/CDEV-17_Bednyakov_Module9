5d7f04099b23cb374bb0cbda49bf699ce5de9680
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-04 23:29:13 +0300

    master: add task 9.4.3

---
+++ Task_9_4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_9_4_3
+{
+    internal class Program
+    {
+        delegate void Human(Chield man);
+        static void Main(string[] args)
+        {
+            Human WhoIsThis = GetParentInfo;
+            WhoIsThis.Invoke(new Chield());
+        }
+        public static void GetParentInfo (Parent person)
+        {
+            Console.WriteLine(person.GetType());
+        }
+    }
+    class Parent
+    {
+
+    }
+    class Chield : Parent
+    {
+
+    }
+}
